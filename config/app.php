31553fa3b872b84e256eef1a3b88a1d37d968acc
guanshuqi <17710293967@163.com>
2019-01-10 14:47:26 +0800

    sss

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY','base64:bwnKCwIzyrvan4Vhn0d0BqD/I5DMI/n7zmefK/FGTwA='),
 
     'cipher' => 'AES-256-CBC',
 
